a599a2ab4840fa6e0b846aa5decfd346a11d9617
semantic-release <semantic-release>
2025-08-28 03:11:30 +0000

    3.3.0
    Automatically generated by python-semantic-release

--- src/logreducer/__init__.py
+++ src/logreducer/__init__.py
@@ -10,7 +10,7 @@ Licensed under the HyperSec EULA: https://hypersec.io/eula
 Author: Derek <noreply@hypersec.io>
 """
 
-__version__ = "3.2.1"
+__version__ = "3.3.0"
 __author__ = "Derek"
 __email__ = "noreply@hypersec.io"
 __license__ = "HyperSec EULA"
